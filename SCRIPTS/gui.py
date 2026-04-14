84a231e5cd7cc532dd139d029585f956e2635346
OssianV <ossian_rmz@hotmail.com>
2026-04-14 17:39:48 -0600

    Se completo el borrador de la implementacion de la valiacion en la GUI. Para terminar solo se necesita el modulo de validacion para ajustar detalles

--- SCRIPTS/gui.py
+++ SCRIPTS/gui.py
@@ -16,10 +16,20 @@ FONT_BODY = ("Century Gothic", 10)
 # Pendientes
 
 # 1. Remplazar el uso de Optional, por simplemente usar el simbolo de disyuncion "|"
-# 2. Al presionar el boton "Regresar" en la ventana de resultados, se tienen que volver a ingresar
-#    los archivos .csv? - Si es asi, se tiene que cambiar esto. - No se tienen que volver a ingresar!
-# 3. Agregar un cuadro de texto que muestre el nombre del .csv seleccionado
+# 2. En ventana inicial: Agregar un cuadro de texto que muestre el nombre del .csv seleccionado
+# 3. En ventana de resultados: Agregar un menu de opciones para seleccionar tipo de analisis
+#        - Simple impresion de valores de razones
+#        - Razon 1: Analisis automatico
+#        - ... Razon 6: Analisis automatico
+# 4. Plantear GIU para ingesta de datos
+# 5. Agregar ventana de arhivos incorrectos, con mesaje de que es lo que tienen incorrecto
+#        - Por el momento, solo mensajes de archivo incorrecto y no permitir avanzar de ventana
 
+# Funciones temporales - repreesntan funciones que se importaran de otros modulos no hechos
+
+# Esta funcion es para probar la implementacion de la validacion en la GUI
+def csv_validation(bg_path: str, er_path: str) -> Dict:
+    return {"BG": (False, "El balance general no respeta la ecuacion contable A=P+C\nEl balance general no respeta regla 1\nEl balance general no respeta regla 2\n"), "ER": (True, "")}
 
 # Clases
 
@@ -29,7 +39,7 @@ class PathAndResults:
         self.bg_csv_path: Optional[str] = None    # Type hint: Indica que el atributo bf_csv_path de la clase ApplicationState es string o None (sin asignar valor).
         self.er_csv_path: Optional[str] = None    # Type hint: Indica que el atributo er_csv_path de la clase ApplicationState es string o None (sin asignar valor).
         self.results: Dict = {}    # Type hint: Indica que el atributo results de la clase ApplicationState es un dict (sin asignar valor).
-    
+        
     def set_bg_path(self, path: str) -> None:
         self.bg_csv_path = path
     
@@ -39,13 +49,23 @@ class PathAndResults:
     def set_results(self, results: Dict) -> None:
         self.results = results
 
+# Hice cambios en las siguientes funciones:
+# csv_validation: Funcion temporal. Esta funcion se importara del modulo de lectura,limpieza y validacion.
+# - start_window : Cambie el comando que llama el boton siguiente; sigue siendo results_window, pero ahora con un argumento extra, por tanto ahora es una lambda function (el argumento es la funcion temporal csv_validation)
+# - dashboard_window : Mismo cambio que en start_window
+# - results_window: Cambios sustanciales
+#       - Agregue un argumento con type hint dict asociado al diccionario de resultados de la validacion de los csv
+#       - Agregue un condicional para mandar mensaje si no se paso la validacion, y continuar el programa si se paso con exito
+
 class ProgramaGui:
     """Clase principal de la GUI del programa"""
 
-    def __init__(self, results_dict: Optional[Dict] = None):
+    def __init__(self, results_dict: Optional[Dict] = None, csv_validation_results_dict: Optional[Dict] = None):
         self.state = PathAndResults()    # El atributo .state de la clase ProgramGui es una instancia de la clase PathAndResults
         if results_dict:
             self.state.set_results(results_dict)
+        if csv_validation_results_dict:
+            self.state.set_csv_validation_results(csv_validation_results_dict)
         self.root: Optional[tk.Tk] = None    # El atributo .root de la clase ProgramGui es una ventana de tkinter o None
     
     def _create_window(self) -> tk.Tk:
@@ -103,7 +123,7 @@ class ProgramaGui:
             # Botones
             bg_button = tk.Button(self.root, text="Selecciona el csv del Balance General", command=lambda: self._select_csv_file("BG"), font = FONT_BODY)
             er_button = tk.Button(self.root, text="Selecciona el csv del Estado de Resultados", command=lambda: self._select_csv_file("ER"), font = FONT_BODY)
-            next_button = tk.Button(self.root, text="Siguiente", command=self.results_window, font=FONT_BODY)
+            next_button = tk.Button(self.root, text="Siguiente", command=lambda: self.results_window(csv_validation(bg_path=self.state.bg_csv_path, er_path=self.state.er_csv_path)), font=FONT_BODY)
             
             # Posicionamiento de widgets
             title_label.grid(row=0, column=0, columnspan=4, padx=20, pady=20)
@@ -116,9 +136,22 @@ class ProgramaGui:
             self._show_error(f"Error en la ventana inicial: {str(e)}")
             logger.exception("Excepcion en la ventana de ingesta de csv")
 
-    def results_window(self) -> None:
+    def results_window(self, csv_validation_results: Dict) -> None:
         """Visualizacion de resultados"""
         try:
+
+            # Antes de crear una nueva ventana y cerrar la actual, se tienen que analizar los resultados de la validacion de los csv
+            
+            if csv_validation_results:
+                error_dict = {}
+                for key, value in csv_validation_results.items():
+                    if value[0] == False:   # No paso la validacion
+                        error_dict[key]=value[1]
+                
+                if error_dict:
+                    self._show_info("Ops. Los archivos .csv no son validos",self._dict_to_string(error_dict))
+                    return None
+
             self._create_window()
             
             # Definicion de widgest de la ventana
@@ -149,7 +182,7 @@ class ProgramaGui:
             self._create_window()
 
             message_label = tk.Label(self.root, text="Dashboard\n(No implementado aun)", font=FONT_TITLE)
-            back_button = tk.Button(self.root, text="Regresar", command=self.results_window, font=FONT_BODY )
+            back_button = tk.Button(self.root, text="Regresar", command=lambda: self.results_window(None), font=FONT_BODY )
 
             message_label.pack(padx=20, pady=20)
             back_button.pack(padx=15, pady=10)
